6177b67faf65858ce832cf2a044e79c5dea93e88
daniloesk <47666332+daniloesk@users.noreply.github.com>
2021-03-29 19:26:27 -0300

    ng generate component Stats

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmptyRouteComponent,
+    StatsComponent,
   ],
   imports: [
     BrowserModule,
